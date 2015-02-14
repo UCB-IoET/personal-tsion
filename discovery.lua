2164d9f62f5dc66647d83c2c6e5dc3b4f942d586
Tsion Behailu <tsionbehailu@gmail.com>
2015-02-14 13:58:44 -0800

    sending invokation correctly

--- discovery.lua
+++ discovery.lua
@@ -1,18 +1,29 @@
-local svc_manifest = {id="ATeam"}              
+require "cord"
+sh = require "stormsh"
 
+table = {}
 
-S_PORT = 1525
+S_PORT = 1526
 
 -- service announcement table
-local svc_manifest ={ id= "Sin Nombre", setRlyA={ s="setBool", desc= "red LED"}, }	
+local svc_manifest ={ id= "Sin Nombre", setRlyA={ s="setBool", desc= "red LED"}}	
 local msg = storm.mp.pack(svc_manifest)
+
+-- service invocation message
+local svc_invoke ={["setRlyA"]=1}
+msg = storm.mp.pack(svc_invoke)  
 
 s_handler = function(payload, from, port) 
 		print (string.format("from %s port %d: %s",from,port,payload))
+		--table[from] = storm.mp.unpack(payload) 
+		table[from] = svc_manifest
+		--msg = storm.mp.pack(svc_invoke)	
 	    end
+
 ssock = storm.net.udpsocket(S_PORT, s_handler)
 
-storm.os.invokePeriodically(5*storm.os.SECOND, function()
+storm.os.invokePeriodically(1*storm.os.SECOND, function()
+	print(msg)
 	storm.net.sendto(ssock, msg, "ff02::1", 1525)
 	print("service table has been sent!")
 end)
@@ -21,3 +32,6 @@ end)
 function svc_stdout(from_ip, from_port, msg)
   print (string.format("[STDOUT] (ip=%s, port=%d) %s", from_ip, from_port, msg))
 end
+
+sh.start()
+cord.enter_loop()
