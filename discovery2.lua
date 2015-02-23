1f1d6b71c98c5a67387dbccce90e2c5c7079b27f
Tsion Behailu <tsionbehailu@gmail.com>
2015-02-23 00:28:21 -0800

    minor changes

--- discovery2.lua
+++ discovery2.lua
@@ -4,8 +4,8 @@ shield = require("starter")
 
 table = {}
 
-S_PORT = 1526
-R_PORT = 1525
+S_PORT = 1626
+R_PORT = 1625
 
 function pprint()
 	print("print table function")
@@ -24,6 +24,30 @@ function pprint()
 	print(" ")
 end
 
+function pt(x) for k,v in pairs(x) do print("", "", k,v) end end
+
+function print_msg(msg)
+   if (type(msg) ~= "table") then
+	  print("Malformed service [type=%s]", type(msg))
+	  return {}
+   end
+
+   for k,v in pairs(msg) do
+	  if (k ~= "id") then
+		 if type(v) ~= "table" then
+			print("", k, v)
+		 else
+			print("", k)
+			pt(v)
+		 end
+	  else
+		 print("", k, v)
+	  end
+   end
+
+   return msg
+end
+
 ----------------------- S Main --------------------------
 
 function s_main()
@@ -34,7 +58,7 @@ function s_main()
         ssock = storm.net.udpsocket(R_PORT, function(payload, from, port)
 						print (string.format("We're not stupid: from %s port %d: %s",from,port,payload))
                                                 table[from] = storm.mp.unpack(payload)
-                                                pprint(table)
+                                                --pprint(table)
 					     end)
 
         serviceSock = storm.net.udpsocket(S_PORT, function(payload, from, port)
@@ -43,39 +67,27 @@ function s_main()
 
 
 
-        storm.os.invokePeriodically(1*storm.os.SECOND, function()
+        storm.os.invokePeriodically(5*storm.os.SECOND, function()
                 print(msg)
                 storm.net.sendto(ssock, msg, "ff02::1", R_PORT)
-                print("service table has been sent!")
-                for location, service_announcement in pairs(table) do
-                        print ("location:", location)
-                        for id, services_offered in pairs(service_announcement) do
-                                print ("id:", id)
-                                if (services_offered.s == "setBool") then
-                                        print("found service of type setBool:", services_offered.desc)
-                                        local svc_invoke = {[id]=1}
-                                        local msg_sent = storm.mp.pack(svc_invoke)
-                                        storm.net.sendto(serviceSock, msg_sent, location, S_PORT)
-                                        print (string.format("to %s port %d: %s", location, S_PORT, msg_sent))
-                                end
-                        end
-                end
-	end)
+		print("service table has been sent!")
+		end)
 end
 ---------------------- R Main --------------------------
 
 function r_main()
 	-- service announcement table
-	local svc_manifest ={ id = "Firestorm2", setRlyA={ s="setBool", desc= "red LED"}}       
+	local svc_manifest ={ id = "Firestorm2", redLED={ s="setBool", desc= "red LED"}}       
 	local msg = storm.mp.pack(svc_manifest)
 
 	-- service invocation message
-        local svc_invoke ={["setRlyA"]=1}
-        msg = storm.mp.pack(svc_invoke)  -- pack service invocation
+        local svc_invoke ={"setLEDblue", {1}}
+        invoke_msg = storm.mp.pack(svc_invoke)  -- pack service invocation
 
 	--- set up socket
 	rsock = storm.net.udpsocket(R_PORT, function(payload, from, port)
-					print (string.format("from %s port %d: %s",from,port,payload))
+					print (string.format("from %s port %d:",from,port))
+					
 					table[from] = storm.mp.unpack(payload)
 					pprint()
 					svc_stdout(from, port, storm.mp.unpack(payload))
@@ -83,21 +95,39 @@ function r_main()
 
 
 	--- send service table
-	storm.os.invokePeriodically(500*storm.os.MILLISECOND, function()
-								storm.net.sendto(rsock, msg, "ff02::1", R_PORT)
-								print("service table has been sent!")
-							      end)
+	storm.os.invokePeriodically(5*storm.os.SECOND, function()
+						storm.net.sendto(rsock, msg, "ff02::1", R_PORT)
+						print("service table has been sent!")
+						for location, service_announcement in pairs(table) do
+							print ("location:", location)
+							for id, services_offered in pairs(service_announcement) do
+								print ("id:", id)
+								--print ("services_offered ", services_offered)
+								if (services_offered.s == "setBool") then
+									print("found service of type setBool:", services_offered.desc)
+									local svc_invoke = {id, {1}}
+									local msg_sent = storm.mp.pack(svc_invoke)
+									storm.net.sendto(rsock, msg_sent, location, S_PORT)
+									print (string.format("to %s port %d: %s", location, S_PORT, msg_sent))
+				                                end
+								--print ("Checked services offered.")
+                       					 end
+                				end
+	      
+					end)
 end
 
 -- service that receives msg and carries out call
 function svc_stdout(from_ip, from_port, msg)
 	--print (string.format("[STDOUT] (ip=%s, port=%d) %s", from_ip, from_port, msg))
-	print(msg.setRlyA)	
-	local invoke = msg.setRlyA
-	if invoke == 1 then
-		shield.LED.on("blue")
-		print("Decoded message!")
-	end
+	--print (string.format("[STDOUT] (ip=%s, port=%d)", from_ip, from_port))
+	--print(msg.setRlyA)	
+	--local invoke = msg.setRlyA
+	--if invoke == 1 then
+	--	shield.LED.on("blue")
+	--	print("Decoded message!")
+	--end
+	print("Decoded message!")
 end
 
 sh.start()
